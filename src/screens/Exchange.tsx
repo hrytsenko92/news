2a6ecad937462425da1b665ea4b5005edec2536b
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-05-05 17:06:57 +0300

    fix exchange screen

--- src/screens/Exchange.tsx
+++ src/screens/Exchange.tsx
@@ -4,22 +4,28 @@ import { apiLoader } from "../api/apiLoader";
 
 export const Exchange: FC = () => {
   const [data, setData] = useState<ExchangeRateType>();
-  const [selectedInCurrency, setSelectedInCurrency] = useState("UAH");
-  const [selectedOutCurrency, setSelectedOutCurrency] = useState("USD");
+  const [selectedInCurrency, setSelectedInCurrency] = useState<number>(100); // input
+  const [selectedOutCurrency, setSelectedOutCurrency] = useState<number | null>(null);
 
   const onCurrencyInChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedInCurrency(event.target.value);
-    console.log(event.target)
-    // getNewData()
+      const selectedValue = Number(event.target.value);
+      const selectedCurrency: [string, number] = Object.entries(data.conversion_rates).find(([key, value]) => value === selectedValue)
+      getNewData(selectedCurrency[0])
+      // setSelectedInCurrency(Number(event.target.value));
+  //     console.log(event.target.value)
+  // console.log(selectedCurrency); 
   };
   const onCurrencyOutChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOutCurrency(event.target.accessKey);
+    setSelectedOutCurrency(Number(event.target.value));
+    console.log(event.target.value)
   };
   const getNewData = async (code: string) => {
     const res: ExchangeRateType = await apiLoader(`https://v6.exchangerate-api.com/v6/600640d62d3baeea55a4dccd/latest/${code}`);
     setData(res);
   };
-
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedInCurrency(Number(event.target.value));
+  };
   useEffect(() => {
     const getData = async () => {
       const res: ExchangeRateType = await apiLoader(
@@ -29,8 +35,7 @@ export const Exchange: FC = () => {
     };
     getData();
   }, []);
-// console.log(selectedInCurrency)
-// console.log(selectedOutCurrency)
+console.log(data)
   return (
     <div className="container">
       {data !== undefined ? (
@@ -42,7 +47,11 @@ export const Exchange: FC = () => {
               </option>
             ))}
           </select>
-
+          <div className="exchangeValue">
+            <input type="text" value={selectedInCurrency} onChange={handleInputChange} />
+            {selectedOutCurrency !== null ?  <div>{`result is: ${selectedInCurrency * selectedOutCurrency} `}</div> : <div>select code</div>}
+          </div>
+          
           <select onChange={onCurrencyOutChange}>
             {Object.entries(data.conversion_rates).map(([currency, value]) => (
               <option key={currency} value={value}>
