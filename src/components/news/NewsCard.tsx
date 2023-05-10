81f78b7a510bd5dd28097938b94cc613cda785b2
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-05-10 18:59:51 +0300

    add styles to newsCard conponent

--- src/components/news/NewsCard.tsx
+++ src/components/news/NewsCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Article } from "../../types/newsType";
 import style from "./newsCard.module.scss";
 import * as dayjs from "dayjs";
+import notFound from '../../assets/not404.jpg'
 dayjs().format();
 
 type PropsData = {
@@ -14,31 +15,31 @@ type PropsData = {
 export const NewsCard = ({ data, isOpen, index, handleIsOpen }: PropsData) => {
   console.log(data);
   const daysFormated = (i: Date) => {
-    return dayjs(i).format("DD MMMM YYYY HH:MM");
+    return dayjs(i).format("DD MMMM HH:MM");
   };
 
   return (
     <div className={style.newCardContainer}>
+      {isOpen === index ? (
+        <button className={style.closeBTN} onClick={() => handleIsOpen(-1)}>
+          close
+        </button>
+      ) : null}
       <div
         className={
           isOpen === index ? style.openContainer : style.closeContainer
         }
         onClick={() => handleIsOpen(index)}
       >
-        <section className={style.header}>
-          <span className={style.headerTitle}>{data.title}</span>
-          <p className={style.headerDescription}>{data.description}</p>
-         
-        </section>
-        <section className={style.main}>
-          <span className={style.mainDate}>
-            {daysFormated(data.publishedAt)}
-          </span>
-          <p className={style.mainContent}>{data.content}</p>
-          <img className={style.mainIMG} src={data.urlToImage} />
-        </section>
+        <span className={style.title}>{data.title}</span>
+        <p className={style.description}>{data.content}</p>
+        {data.author ? <span className={style.author}>{data.author}</span>: null}
+        <span className={style.infoWrapper}>
+          <span className={style.date}>{daysFormated(data.publishedAt)}</span>
+          <a className={style.readMore} href={data.url} target="_blank">Read more...</a>
+        </span>
+        {data.urlToImage ?  <img className={style.img} src={data.urlToImage} /> :  <img className={style.img} src={notFound} /> }
       </div>
-       <button onClick={() => handleIsOpen(-1)}>close</button>
     </div>
   );
 };
