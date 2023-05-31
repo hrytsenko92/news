82b2011e4f90f066eb1e52b62565444877630763
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-05-31 19:36:55 +0300

    new itemCard component, fix styles

---
+++ src/components/news/ItemCard.tsx
@@ -0,0 +1,45 @@
+import { Article } from "../../types/newsType";
+import style from "./itemCard.module.scss";
+import * as dayjs from "dayjs";
+import notFound from "../../assets/not404.jpg";
+import rightSVG from "../../assets/right.svg";
+dayjs().format();
+
+type PropsData = {
+  data: Article;
+};
+
+export const ItemCard = ({ data }: PropsData) => {
+  const daysFormated = (i: Date) => {
+    return dayjs(i).format("DD MMMM HH:MM");
+  };
+
+  return (
+    <div className={style.itemCardContainer}>
+      {data.urlToImage ? (
+        <img className={style.img} src={data.urlToImage} />
+      ) : (
+        <img className={style.img} src={notFound} />
+      )}
+      <span className={style.title}>{data.title}</span>
+      {data.content !== null ? (
+        <p className={style.description}>
+          {data.content.replace(/\[\+\d+ chars\]/g, "")}
+        </p>
+      ) : null}
+
+      <div className={style.infoWrapper}>
+        <div className={style.authorDate}>
+          {data.author && data.author.length < 15 ? (
+            <span className={style.author}>{data.author}</span>
+          ) : null}
+          <span className={style.date}>{daysFormated(data.publishedAt)}</span>
+        </div>
+        <a className={style.readMore} href={data.url} target="_blank">
+          Read more
+          <img className={style.rightSVG} src={rightSVG} />
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/screens/news/News.tsx
+++ src/screens/news/News.tsx
@@ -1,42 +1,36 @@
-import { FC, Suspense, useState } from "react";
+import { FC, Suspense } from "react";
 import { NewsType, Article } from "../../types/newsType";
 import { apiLoader } from "../../loaders/apiLoader";
 import { defer, useLoaderData, Await } from "react-router-dom";
-import { NewsCard } from "../../components/news/NewsCard";
+import { ItemCard } from "../../components/news/ItemCard";
 import style from "./news.module.scss";
 
 export const newsDataLoader = async () => {
-  const request =
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=782a7379df92415ebe4dc42d9983fc99";
+  const request = "https://newsapi.org/v2/top-headlines?country=us&apiKey=782a7379df92415ebe4dc42d9983fc99";
   const data: NewsType = await apiLoader(request);
   return defer({ data });
 };
 
 export const News: FC = () => {
   const { data } = useLoaderData();
-  const [isOpen, setIsOpen] = useState<number>(-1);
-  const handleIsOpen = (id: number) => {
-    setIsOpen(id);
-  };
   return (
     <div className={style.newsContainer}>
-      <Suspense fallback={<p>suspense loading...</p>}>
-        <Await
-          resolve={data}
-          errorElement={<p>error...</p>}
-          children={(data) =>
-            data.articles.map((item: Article, index: number) => (
-              <NewsCard
-                data={item}
-                isOpen={isOpen}
-                handleIsOpen={handleIsOpen}
-                index={index}
-                key={index}
-              />
-            ))
-          }
-        />
-      </Suspense>
+      <div className={style.titleWrapper}>
+        <h2 className={style.title}>Latest Updates</h2>
+      </div>
+      <div className={style.listWrapper}>
+        <Suspense fallback={<p>suspense loading...</p>}>
+          <Await
+            resolve={data}
+            errorElement={<p>error...</p>}
+            children={(data) =>
+              data.articles.map((item: Article, index: number) => (
+                <ItemCard data={item} key={index}/>
+              ))
+            }
+          />
+        </Suspense>
+      </div>
     </div>
   );
 };
